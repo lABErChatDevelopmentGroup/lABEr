619ac2f0d5529fca24ce4e73278f1016bc02858e
Fabian Geiselhart <me@f4814n.de>
2016-10-15 14:05:31 +0200

    Sollte funktionieren

--- the_network.py
+++ the_network.py
@@ -3,9 +3,33 @@ lABEr
 =====
 The ChatServer for lABEr.
 """
+import threading
+import serwork as sw
+
+def send(server_ip):
+    sc = sw.SWClient((server_ip, 2911))
+    sc.sendData(input("Nachricht: "))
+
+def recive():
+    ip = sw.getMyIp()
+    print("launching server on " + ip)
+    ts = sw.SWServer((ip, 2911))
+    ts.run(msgP, 1024)
+
+def msgP(data, hosts):
+    print(data)
+    return ""
 
 from serwork import *
 
 class ChatServer:
     "The ChatServer for lABEr"
-    pass
+    def __init(self):
+        pass
+
+    def startchat(server_ip):
+        recive_t = threading.Thread(None, recive)
+        send_t = threading.Thread(None, send, None, (server_ip,))
+
+        recive_t.start()
+        send_t.start()
