54df4504556cdbc8d42bf8d5c0fef2dd178eddd8
zoxuyu <maus@paulstrobach.de>
2016-10-15 11:59:02 +0200

    Editing...

--- server_test.py
+++ server_test.py
@@ -1,6 +1,7 @@
 import serwork as sw
-print("launching on " + sw.getMyIp())
-ts = sw.SWServer((sw.getMyIp(), 2911))
+ip = sw.getMyIp()
+print("launching on " + ip)
+ts = sw.SWServer((ip, 2911))
 def msgP(data, hosts):
     print(data)
     return ""
