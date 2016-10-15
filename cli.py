0ff8e9accefc6f518047714af184f8176965c839
Fabian Geiselhart <me@f4814n.de>
2016-10-15 18:21:06 +0200

    Cli client now useable

--- cli.py
+++ cli.py
@@ -3,17 +3,19 @@ from networking import ChatServer
 import serwork as sw
 import sys
 
+def printOut(msg, data):
+    msg = msg.split("~:split:~")
+    print(msg[0] + ": " + msg[1])
+    return ""
+
 other_ips = sys.argv[1:]
 ip = sw.getMyIp()
 
 print("Your IP: " + ip)
 username = input("Your Username: ")
 print("--------------------------------------------------------------------------------------------")
-def printOut(msg, data):
-    msg = msg.split("~:split:~")
-    print(msg[0] + ": " + msg[1])
-    return ""
+
 ChatServer.startchat(printOut)
 while True:
-    inp = input("")
+    inp = input()
     ChatServer.send(username, inp, other_ips, ip)
