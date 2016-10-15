a2bad9d964fa54770acc82667f84ea663a981cb0
zoxuyu <maus@paulstrobach.de>
2016-10-15 11:49:56 +0200

    Testing....

--- server_test.py
+++ server_test.py
@@ -1,4 +1,5 @@
 import serwork as sw
+print("launching on " + sw.getMyIp())
 ts = sw.SWServer((sw.getMyIp(), 2911))
 def msgP(data, hosts):
     print(data)
--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 from ipaddress import IPv4Network
 import serwork as sw
-ip_ =  sw.getMyIp()
-ip = IPv4Network(ip_)
-brad = str(ip.broadcast_address)
-print(brad)
 
-sc = sw.SWClient((brad, 2911))
+ip = input("Ip: ")
+
+sc = sw.SWClient((ip, 2911))
 sc.sendData("Hallo!")
