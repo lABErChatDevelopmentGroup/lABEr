b88022378204abca19afb36ad0549f5496079bdf
Fabian Geiselhart <me@f4814n.de>
2016-10-15 16:51:38 +0200

    Deleted useless files

--- server_test.py
+++
@@ -1,8 +0,0 @@
-import serwork as sw
-ip = sw.getMyIp()
-print("launching on " + ip)
-ts = sw.SWServer((ip, 2911))
-def msgP(data, hosts):
-    print(data)
-    return ""
-ts.run(msgP, 1024)
--- test.py
+++
@@ -1,8 +0,0 @@
-from ipaddress import IPv4Network
-import serwork as sw
-
-#ip = input("Ip: ")
-ip = "100.100.247.1"
-
-sc = sw.SWClient((ip, 2911))
-sc.sendData(input("Nachricht: "))
