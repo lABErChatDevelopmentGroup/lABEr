4eedd8397060dae8a4ea9a2f089a98613e9f9cae
zoxuyu <maus@paulstrobach.de>
2016-10-15 13:07:45 +0200

    ...

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from ipaddress import IPv4Network
 import serwork as sw
 
-ip = input("Ip: ")
+#ip = input("Ip: ")
+ip = "100.100.247.1"
 
 sc = sw.SWClient((ip, 2911))
-sc.sendData("Hallo!")
+sc.sendData(input("Nachricht: "))
